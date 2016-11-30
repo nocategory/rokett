1fece0a733afe3bc11d21f54b72f8a9111968cb7
uppe-r <joaozinn98@gmail.com>
2016-11-30 11:35:44 +0000

    started working on side panel for files

--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -1,12 +1,13 @@
 // @flow
 import React, { Component } from 'react';
-import s from './Sidebar.css';
+// import SplitPane from 'react-split-pane';
+// import s from './Sidebar.css';
 
 export default class Sidebar extends Component {
   render() {
     return (
       /* sidebar for files */
-      <div className={`${s['app--sidebar']} non--draggable`} />
+      <div />
     );
   }
 }
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,7 +1,13 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
+import SplitPane from 'react-split-pane';
 import DraggableArea from '../components/DraggableArea/DraggableArea';
 import Sidebar from '../components/Sidebar/Sidebar';
+import s from '../components/Sidebar/Sidebar.css';
+
+const maxSizePane = {
+  maxWidth: '25%',
+};
 
 
 export default class App extends Component {
@@ -12,13 +18,22 @@ export default class App extends Component {
   render() {
     return (
       <div className="app--wrapper">
-        <Sidebar />
-        <div className="app--content--wrapper">
-          <DraggableArea />
-          <div className="app--content" id="app--content">
-            {this.props.children}
+
+        <SplitPane split="vertical" minSize={200} defaultSize={200} pane1Style={maxSizePane} className={'non--draggable'}>
+          {/* pane1 */}
+          <div className={`${s['app--sidebar']}`} />
+
+          {/* pane2 */}
+          <div className="app--content--wrapper">
+            <DraggableArea />
+            <div className="app--content" id="app--content">
+              {this.props.children}
+            </div>
           </div>
-        </div>
+        </SplitPane>
+        <Sidebar />
+        <span className="Resizer" />
+
       </div>
     );
   }
