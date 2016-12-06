b61e68cc6756bf36e2189b8867829da211629f25
root <root>
2016-12-06 01:14:00 +0000

    changed sidebar color && added settings button && new icon hover properties

--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -2,13 +2,18 @@
 import React, { Component } from 'react';
 import s from './Sidebar.css';
 
+const settingsButton = {
+  fontSize: '2.2em',
+};
 
 export default class Sidebar extends Component {
   render() {
     return (
       <div className={s.sidebarWrapper}>
         <div className={s.sidebar}>
-          ee
+          <div className={s.sidebarButtons}>
+            <i className="fa fa-cog" aria-hidden="true" style={settingsButton} />
+          </div>
         </div>
       </div>
     );
