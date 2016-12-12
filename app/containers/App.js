4b9e9634f2def962666b292c0db8d558133c10e2
uPPer <joaozinn98@gmail.com>
2016-12-12 13:15:34 +0000

    added working file tree (treebeard), modified directory-tree npm module

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,10 +2,13 @@
 import React, { Component, PropTypes } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import dirTree from 'directory-tree';
+import walk from 'walkdir';
+import p from 'path';
 import Sidebar from '../components/Sidebar/Sidebar';
 import TopHeader from '../components/TopHeader/TopHeader';
 import FrameButtons from '../components/FrameButtons/FrameButtons';
 import Modal from '../components/Modal/Modal';
+import { Treebeard } from 'react-treebeard';
 
 // // // // // // // //
 // import fs from 'fs-extra';
@@ -25,20 +28,27 @@ export default class App extends Component {
 
   constructor() {
     super();
-    this.state = {
-      folders: '',
-      files: '',
-    };
+    this.state = {};
+    this.onToggle = this.onToggle.bind(this);
   }
 
   componentWillMount() {
-    this.tree = dirTree('C:\\Users\\joaosalg\\Desktop\\quarkz\\app\\components');
+    this.tree = dirTree('L:\\Users\\joaoz\\Desktop\\quarkz\\app\\components');
     console.log(this.tree);
+    //
+    // this.tt('L:\\Users\\joaoz\\Desktop\\quarkz\\app\\components', 'filetree');
   }
 
   componentDidMount() {
-    this.getFilesandFolders(this.tree);
+    // this.getFilesandFolders(this.tree);
   }
+
+  onToggle(node, toggled) {
+    if(this.state.cursor){this.state.cursor.active = false;}
+    node.active = true;
+    if(node.children){ node.toggled = toggled; }
+    this.setState({ cursor: node });
+   }
 
   getFilesandFolders(y) {
     for (let i = 0, len = y.children.length; i < len; i += 1) {
@@ -50,7 +60,7 @@ export default class App extends Component {
         if (y.children[i].path.indexOf(lastPath) === -1 || lastPath === '') {
           console.log(y.children[i].path + ' and ' + lastPath);
           fileFolders.push(
-            <p className="tree-folder expanded" data-path={y.children[i].path} onClick={() => console.log('click')}>
+            <p className="tree-folder expanded" key={y.children[i].path} data-path={y.children[i].path} onClick={() => console.log('click')}>
               <i className="fa fa-folder" aria-hidden="true" />
               {y.children[i].name}
             </p>
@@ -59,7 +69,7 @@ export default class App extends Component {
         else {
           console.log(y.children[i].path + ' and ' + lastPath);
           fileFolders.push(
-            <p className="tree-folder children collapsed" data-path={y.children[i].path} onClick={() => console.log('click')}>
+            <p className="tree-folder children collapsed" key={y.children[i].path} data-path={y.children[i].path} onClick={() => console.log('click')}>
               <i className="fa fa-folder" aria-hidden="true" />
               {y.children[i].name}
             </p>
@@ -72,7 +82,7 @@ export default class App extends Component {
       else { // @file
         console.log(`file: ${y.children[i].name}`);
         fileFolders.push(
-          <p className="tree-folder children collapsed" data-path={y.children[i].path} onClick={() => console.log('click')}>
+          <p className="tree-folder children collapsed" key={y.children[i].path} data-path={y.children[i].path} onClick={() => console.log('click')}>
             <i className="fa fa-file" aria-hidden="true" />
             {y.children[i].name}
           </p>
@@ -82,6 +92,35 @@ export default class App extends Component {
     // return [folders, files];
     return this.setState({
       filetree: fileFolders,
+    });
+  }
+
+  tt(y, x) {
+    let emitter = walk(y, { "no_recurse": true });
+    emitter.on('file', (filename, stat) => {
+      console.log('file from emitter: ', filename);
+      fileFolders.push(
+        <p className="tree-folder children collapsed" key={filename} data-path={filename} onClick={() => console.log('click')}>
+          <i className="fa fa-file" aria-hidden="true" />
+          {p.basename(filename)}
+        </p>
+      );
+    });
+    emitter.on('directory', (path, stat) => {
+      let name = p.basename(path);
+      console.log('directory from emitter: ', path);
+      fileFolders.push(
+        <ol className="tree-folder expanded" key={path} data-path={path} onClick={() => tt(path, )}>
+          <i className="fa fa-folder" aria-hidden="true" />
+          {name}
+          {this.state.name}
+        </ol>
+      );
+    });
+    emitter.on('end', () => {
+      return this.setState({
+        filetree: fileFolders,
+      });
     });
   }
 
@@ -103,9 +142,14 @@ export default class App extends Component {
 
             {/* pane 1 */}
             <div className="app--tree">
-              <div>{this.state.filetree}</div>
+              <div>
+                <Treebeard
+                  data={this.tree}
+                  onToggle={this.onToggle}
+                />
+              </div>
               <div className="tree-chevron-wrapper">
-                <i className="fa fa-chevron-left" style={treeChevron} />
+                <i className="fa fa-chevron-right" style={treeChevron} />
               </div>
             </div>
 
