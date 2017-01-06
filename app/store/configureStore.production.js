ca7d3d9eb5beb32dd01e242b8039909c660c3770
uPPer <joaozinn98@gmail.com>
2017-01-06 00:12:46 +0000

    fixed redux persist not working on prod

--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -1,13 +1,35 @@
 // @flow
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { hashHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
+import { routerMiddleware, push } from 'react-router-redux';
+import { autoRehydrate } from 'redux-persist';
 import rootReducer from '../reducers';
+
+import * as modalActions from '../actions/modal';
+import * as editorActions from '../actions/editor';
+
+
+const actionCreators = {
+  ...modalActions,
+  ...editorActions,
+  push,
+};
 
 const router = routerMiddleware(hashHistory);
 
-const enhancer = applyMiddleware(thunk, router);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    // Options: http://zalmoxisus.github.io/redux-devtools-extension/API/Arguments.html
+    actionCreators,
+  }) :
+  compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, router),
+  autoRehydrate(),
+);
 
 export default function configureStore(initialState: Object) {
   return createStore(rootReducer, initialState, enhancer);
