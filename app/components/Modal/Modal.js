4e75b62fa0d98a4d2ea5eb74095afd43d7b5db22
uppe-r <joaozinn98@gmail.com>
2017-05-10 15:14:55 +0100

    Redesigned modal

--- app/components/Modal/Modal.js
+++ app/components/Modal/Modal.js
@@ -15,64 +15,58 @@ export default class Modal extends Component {
     const { toggleModal, modalVisible, selectSection, selectedSection } = this.props;
     // @TODO: actual settings with section names n stuff
     return (
-      <div className="flex-vertical">
-        <Transition
-          component={false} // don't use a wrapping component
-          enter={{
-            opacity: 1,
-            scale: 1,
-          }}
-          leave={{
-            opacity: 0,
-            scale: 0.6,
-          }}
-        >
-          {modalVisible &&
-            <div className={s.modal} key={1}>
-              <div className="flex-horizontal h100">
-                <div className={s.sidebarWrapper}>
-                  <div className={s.sidebar}>
-                    <div className={s.sectionWrapper}>
-                      <div className={s.sectionLink}>
-                        <div className={s.sectionHeader}>Editor settings</div>
-                        <div className={selectedSection === 'test1' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test1')}>test1</div>
-                        <div className={selectedSection === 'test2' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test2')}>test2</div>
-                      </div>
-                      <div className={s.sectionLink}>
-                        <div className={s.sectionHeader}>UI settings</div>
-                        <div className={selectedSection === 'test3' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test3')}>test3</div>
-                        <div className={selectedSection === 'test4' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test4')}>test4</div>
-                      </div>
-                      <div className={s.sectionLink}>
-                        <div className={s.sectionHeader}>User settings</div>
-                        <div className={selectedSection === 'test5' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test5')}>test5</div>
-                        <div className={selectedSection === 'test6' ? classNames(s.sectionSpan, s.selectedSectionSpan) : s.sectionSpan} onClick={() => selectSection('test6')}>test6</div>
-                      </div>
-                    </div>
+      <Transition
+        component={false} // don't use a wrapping component
+        enter={{
+            // translateY: 0,
+          scale: 1,
+          opacity: 1,
+        }}
+        leave={{
+            // translateY: -window.innerHeight,
+          scale: 0.8,
+          opacity: 0,
+        }}
+      >
+        {modalVisible &&
+        <div className={s.modal} key={1}>
+          <div className="flex-vertical h100">
+            <div className={s.sidebarWrapper}>
+              <div className={s.sidebar}>
+                <div className={s.sectionWrapper}>
+                  <div className={s.sectionLink}>
+                    <div className={selectedSection === 'test1' ? classNames(s.sectionHeader, s.selectedSectionHeader) : s.sectionHeader} onClick={() => selectSection('test1')}>Editor</div>
+                  </div>
+                  <div className={s.sectionLink}>
+                    <div className={selectedSection === 'test2' ? classNames(s.sectionHeader, s.selectedSectionHeader) : s.sectionHeader} onClick={() => selectSection('test2')}>UI</div>
+                  </div>
+                  <div className={s.sectionLink}>
+                    <div className={selectedSection === 'test3' ? classNames(s.sectionHeader, s.selectedSectionHeader) : s.sectionHeader} onClick={() => selectSection('test3')}>User</div>
                   </div>
                 </div>
-                <div className={s.contentWrapper}>
-                  <div className={s.content}>
-                    {/* @TODO: add other sections */}
-                    {selectedSection === 'test1' ?
-                      <p>
-                        <b>Sorry, not ready yet! 1</b>
-                      </p> : selectedSection === 'test2' ?
-                        <p>
+              </div>
+            </div>
+            <div className={s.contentWrapper}>
+              <div className={s.content}>
+                {/* @TODO: add other sections */}
+                {selectedSection === 'test1' ?
+                  <p>
+                    <b>Sorry, not ready yet! 1</b>
+                  </p> : selectedSection === 'test2' ?
+                    <p>
                           <b>Sorry, not ready yet! 2</b>
                         </p> :
                         <p>fail</p>
                     }
-                  </div>
-                </div>
-                <div className={s.exitModal}>
-                  <div className={classNames(s.exitModalIcon)} />
-                </div>
               </div>
             </div>
+            {/* <div className={s.exitModal}>
+                  <div className={classNames(s.exitModalIcon)} />
+                </div>*/}
+          </div>
+        </div>
         }
-        </Transition>
-      </div>
+      </Transition>
     );
   }
 }
