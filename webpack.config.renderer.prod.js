aa8ada9508442304f1312221b3e228a9e57a6a8c
uppe-r <joaozinn98@gmail.com>
2017-05-31 12:22:23 +0100

    Disabled webpack __dirname processing

--- webpack.config.renderer.prod.js
+++ webpack.config.renderer.prod.js
@@ -183,4 +183,14 @@ export default merge.smart(baseConfig, {
       template: 'app/app.html'
     })
   ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false
+  },
 });
